9aea2e7974d2fd2a3f5800a5a45406157907a3b4
Dmitry <dmitry@dmitry.(none)>
2014-03-01 11:53:28 +0400

    Remove ActionBar in LoginActivity

--- src/com/keysofpeace/app/ui/LoginActivity.java
+++ src/com/keysofpeace/app/ui/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.keysofpeace.app.ui;
 
 import com.keysofpeace.app.R;
-import com.keysofpeace.app.R.layout;
-import com.keysofpeace.app.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
